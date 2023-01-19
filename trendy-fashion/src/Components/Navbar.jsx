03bff013e8c36bbf6dfd3e576cecac5f9a60d2b0
kaushalragini <ragini11.93@gmail.com>
2023-01-19 22:11:57 +0530

    addd

--- trendy-fashion/src/Components/Navbar.jsx
+++ trendy-fashion/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Navbar.css"
-import { Box,   Hide,   Image,  Input,      Popover, PopoverContent, PopoverTrigger } from '@chakra-ui/react'
+import { Box, Hide, Image, Input, Popover, PopoverContent, PopoverTrigger } from '@chakra-ui/react'
 import Men from './Men';
 import Women from './Women';
 import Kids from './Kids';
@@ -12,38 +12,38 @@ import { Link } from 'react-router-dom';
 import DrawerExample from './Drawer';
 import SearchInput from './SearchInput';
 // import {HamburgerIcon} from "@chakra-ui/icons";s
-import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   // const [clicked,setClicked] = useState(true);
   return (
     <>
-    <Box id='Navbar' >
+      <Box id='Navbar' >
         <Box id='NavbarTop' >
-             <ul>
-             <Link to='/myAccount' ><li>MyAccount</li></Link>
-                <li><Sigin/></li>
-                <li>SignUp/Login</li>
-                <li>Customer Care</li>
-                <li>Visit AJIOLUXE</li>
-             </ul>
+          <ul>
+            <Link to='/myAccount' ><li>MyAccount</li></Link>
+            <li><Sigin /></li>
+            <li>SignUp/Login</li>
+            <li>Customer Care</li>
+            <li>Visit AJIOLUXE</li>
+          </ul>
         </Box>
-    <Box id='NavbarBottom'  >
-      <Link to="/" >  <Box  >
-            <img className='project-Logo' src="https://user-images.githubusercontent.com/107903370/212881832-6209377a-42ad-4057-a973-6ecba2dcd60d.png" alt="project-Logo"  />
-        </Box>
-        </Link>
-       
-        <Box id='NavBotLi'  >
-        <Hide below="md">
-        <Popover matchWidth={false} trigger="hover">
+        <Box id='NavbarBottom'  >
+          <Link to="/" >  <Box  >
+            <img className='project-Logo' src="https://user-images.githubusercontent.com/107903370/212881832-6209377a-42ad-4057-a973-6ecba2dcd60d.png" alt="project-Logo" />
+          </Box>
+          </Link>
+
+          <Box id='NavBotLi'  >
+            <Hide below="md">
+              <Popover matchWidth={false} trigger="hover">
                 <PopoverTrigger>
-                <Link to='/men/:param' > <span>MEN</span></Link> 
+                  <Link to='/men/:param' > <span>MEN</span></Link>
                 </PopoverTrigger>
                 <PopoverContent w="100%" mt="10px">
                   <Men />
                 </PopoverContent>
               </Popover>
-              
+
               <Popover matchWidth={false} trigger="hover">
                 <PopoverTrigger>
                   <span>WOMEN</span>
@@ -57,7 +57,7 @@ const Navbar = () => {
                   <span>KIDS</span>
                 </PopoverTrigger>
                 <PopoverContent w="100%" mt="10px">
-                  <Kids/>
+                  <Kids />
                 </PopoverContent>
               </Popover>
               <Popover matchWidth={false} trigger="hover">
@@ -79,37 +79,37 @@ const Navbar = () => {
 
               <Popover matchWidth={false} trigger="hover">
                 <PopoverTrigger>
-                <Input placeholder='Search AJIO'  />
+                  <Input placeholder='Search AJIO' />
                 </PopoverTrigger>
                 <PopoverContent w="100%" mt="10px">
                   <SearchInput />
                 </PopoverContent>
               </Popover>
-           
-           
-         
-         
-        
-        
-          <Image paddingBottom ="10px" src="https://assets.ajio.com/static/img/wishlistIcon.svg" />
-          
 
-          <Image src="https://cdn-icons-png.flaticon.com/512/71/71200.png" style={{width:"3%",height:"4%"}} />
-         
-          </Hide>
-         <div  >
-         {/* <i id="bar" className={clicked?'fas fa-bars':'fas fa-times'} onClick={()=>{setClicked(!clicked)}} ></i>  */}
-         <DrawerExample/>
-         
-         </div>
-          {/* {<HamburgerIcon boxSize="1.5em" />} */}
-          
+
+
+
+
+
+              <Image paddingBottom="10px" src="https://assets.ajio.com/static/img/wishlistIcon.svg" />
+
+
+              <Image src="https://cdn-icons-png.flaticon.com/512/71/71200.png" style={{ width: "3%", height: "4%" }} />
+
+            </Hide>
+            <div  >
+              {/* <i id="bar" className={clicked?'fas fa-bars':'fas fa-times'} onClick={()=>{setClicked(!clicked)}} ></i>  */}
+              <DrawerExample />
+
+            </div>
+            {/* {<HamburgerIcon boxSize="1.5em" />} */}
+
+          </Box>
+
+
+
         </Box>
-       
-       
-       
-        </Box>
-    </Box>
+      </Box>
     </>
   )
 }
