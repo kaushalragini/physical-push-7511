a1c86356bdd7b37e3949d42006354358602ca0bf
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-01-20 22:07:37 +0530

    login

--- trendy-fashion/src/Pages/MainRoutes.jsx
+++ trendy-fashion/src/Pages/MainRoutes.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./HomePage"
-import Login from './Login'
+
 import ProductPage from './ProductPage'
 import MyAccount from './MyAccount/MyAccount'
 import MenHomePage from './MenHomePage'
 import WomenHomePage from './WomenHomePage'
+import Dashboard from '../Components/Dashboard'
+import Login from './Login'
 const MainRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<HomePage />} />
-            <Route path='/login' element={<Login />} />
+            
             <Route path='/menpage' element={<MenHomePage />} />
             <Route path='/womenpage' element={<WomenHomePage />} />
             <Route path='/payment' element={""} />
             <Route path='/men/:param' element={<ProductPage />} />
             <Route path='/myAccount' element={<MyAccount/>} />
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
         </Routes>
     )
 }
